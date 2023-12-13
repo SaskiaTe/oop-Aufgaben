adee40263f8a3edb637e1f6c0c41126746d52821
SaskiaTe <saskia.tellenbach16@gmail.ch>
2023-12-13 16:57:49 +0100

    day 2 17.00

---
+++ Warenkorb/src/main/java/com/example/warenkorb/OrderItem.java
@@ -0,0 +1,11 @@
+package com.example.warenkorb;
+
+public class OrderItem {
+    int Quantity;
+
+    Product p = new Product();
+
+    public double GetAmount(){
+        return 0.0;
+    }
+}
--- Warenkorb/src/main/java/com/example/warenkorb/Product.java
+++ Warenkorb/src/main/java/com/example/warenkorb/Product.java
@@ -1,4 +1,22 @@
 package com.example.warenkorb;
 
 public class Product {
+    int Id;
+    String Name;
+    Double Price;
+
+
+
+    public void setId(int id) {
+    }
+
+    public void getId() {
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getName() {
+        return Name;
+    }
 }
